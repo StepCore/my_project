ce05716ccf10051c948191aa8701e46210de5d23
Stepan <galantov251016@gmail.com>
2024-06-30 20:04:49 +0500

    get_mask ready

---
+++ widget.py
@@ -0,0 +1,20 @@
+def mask_account_card(string: str) -> str:
+    """Функция, которая возвращает маску карты."""
+    card = []
+    string = string.split()
+    for i in string:
+        if i.isdigit():
+            masked_num = i[:-10] + '******' + i[-4:]
+        elif i == 'Счет':
+            return 'Счет **' + string[1][-4:]
+        else:
+            card.append(i)
+    for i in range(0, len(masked_num), 4):
+        card.append(masked_num[i : i + 4])
+    return ' '.join(card)
+
+
+print(mask_account_card('Visa Gold 5999414228426353'))
+
+
+def get_data
