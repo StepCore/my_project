62ad52eb02de4800465e1776a9f2ab8a81fc68fd
Stepan <galantov251016@gmail.com>
2024-07-02 02:02:42 +0500

    update

--- widget.py
+++ widget.py
@@ -1,7 +1,7 @@
-from project_2 import *
+from project_2 import get_mask_account, get_mask_card_number
 
 
-def mask_card(card_number: str) -> str:
+def mask_account_card(card_number: str) -> str:
     """Функция, которая возвращает маску карты."""
     card = []
     name = []
@@ -18,12 +18,6 @@ def mask_card(card_number: str) -> str:
     return " ".join(name) + " " + get_mask_card_number(" ".join(card))
 
 
-print(mask_card("Visa Gold 5999414228426353"))
-
-
 def get_data(account_number: str) -> str:
     """Функция, которая возвращает корректную дату"""
     return account_number[8:10] + "." + account_number[5:7] + "." + account_number[:4]
-
-
-print(get_data("2018-07-11T02:26:18.671407"))
