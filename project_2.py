f745581f14eff61cd218e7ed311ab97c4b5663bb
Stepan <galantov251016@gmail.com>
2024-07-01 16:34:13 +0500

    update project_2.py

--- project_2.py
+++ project_2.py
@@ -3,16 +3,16 @@ def get_mask_card_number(num: str) -> str:
     card = []
     num = str(num).replace(str(num)[6:12], "******")
     for i in range(0, len(num), 4):
-        card.append(num[i : i + 4])
+        card.append(num[i: i + 4])
     return " ".join(card)
 
 
-print(get_mask_card_number('7000792289606361'))
+print(get_mask_card_number("7000792289606361"))
 
 
 def get_mask_account(num: str) -> str:
     """Функция, которая возвращает маску счета."""
-    return '**' + num[-4:]
+    return "**" + num[-4:]
 
 
-print(get_mask_account('1256412318697421'))
+print(get_mask_account("1256412318697421"))
