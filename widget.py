25fac9906464fcabe921ca19ef5965fc7b604a23
Stepan <galantov251016@gmail.com>
2024-07-01 22:02:39 +0500

    update widget.py

--- widget.py
+++ widget.py
@@ -1,15 +1,15 @@
 from typing import Any
 
 
-def mask_account_card(string: Any) -> str:
+def mask_account_card(card_number: str) -> str:
     """Функция, которая возвращает маску карты."""
     card = []
-    string = string.split()
-    for i in string:
+    card_number = card_number.split()
+    for i in card_number:
         if i.isdigit():
             masked_num = i[:-10] + "******" + i[-4:]
         elif i == "Счет":
-            return "Счет **" + string[1][-4:]
+            return "Счет **" + card_number[1][-4:]
         else:
             card.append(i)
     for i in range(0, len(masked_num), 4):
@@ -17,12 +17,12 @@ def mask_account_card(string: Any) -> str:
     return " ".join(card)
 
 
-print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Visa Platinum 5999414228426353"))
 
 
-def get_data(string: str) -> str:
+def get_data(account_number: str) -> str:
     '''Функция, которая возвращает корректную дату'''
-    return string[8:10] + "." + string[5:7] + "." + string[:4]
+    return account_number[8:10] + "." + account_number[5:7] + "." + account_number[:4]
 
 
 print(get_data("2018-07-11T02:26:18.671407"))
