3a37379c585cda8bb6b58d0628ce41ceb1ee3e01
Stepan <galantov251016@gmail.com>
2024-07-04 18:14:42 +0500

    masks ready

--- project_2/src/masks.py
+++ project_2/src/masks.py
@@ -1,10 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая возвращает маску карты."""
     card = []
-    card_number = card_number.replace(' ', '')
+    card_number = card_number.replace(" ", "")
     card_number = card_number.replace(card_number[6:12], "******")
     for i in range(0, len(card_number), 4):
-        card.append(card_number[i: i + 4])
+        card.append(card_number[i : i + 4])
     return " ".join(card)
 
 
