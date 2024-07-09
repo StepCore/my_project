532fc413c0ec366df30f2b1a87233e3051a4d0a8
Stepan <galantov251016@gmail.com>
2024-07-10 01:04:27 +0500

    update

--- project_2/src/processing.py
+++ project_2/src/processing.py
@@ -5,6 +5,7 @@ def filter_by_state(
     list_of_data: Any,
     state="EXECUTED",
 ) -> Any:
+    '''Функция, которая возвращает список словарей, отсортированный по категории "state"'''
     right_list = []
     for dict in list_of_data:
         if dict["state"] == state:
@@ -12,5 +13,6 @@ def filter_by_state(
     return right_list
 
 
-def sort_by_date(list_of_data: Any, key="date", reverse=False) -> Any:
+def sort_by_date(list_of_data: Any, key="date", reverse=True) -> Any:
+    '''Функция, которая возвращает список словарей, отсортированный по дате'''
     return sorted(list_of_data, key=lambda x: x[key], reverse=reverse)
