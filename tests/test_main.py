be55227fee7c99abe357e2f19251474e5ac79f08
Stepan <galantov251016@gmail.com>
2024-07-13 00:54:04 +0500

    test for masks and widget

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
+from widget import get_data, mask_account_card
 
 
 @pytest.mark.parametrize(
@@ -25,3 +30,11 @@ def test_get_mask_card_number(value, expected):
 )
 def test_get_mask_account(value, expected):
     assert get_mask_account(value) == expected
+
+@pytest.mark.parametrize('value, expected', [('Visa Classic 7895325844526588', 'Visa Classic 7895 32** **** 6588'), ('MasterCard 7892 4567 7841 5562', 'MasterCard 7892 45** **** 5562'), ('Счет 786432489 410984123', 'Счет **4123')])
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
+
+@pytest.mark.parametrize('value, expected', [('2024-03-11T02:26:18.671407', '11.03.2024'), ('','')])
+def test_get_data(value, expected):
+    assert get_data(value) == expected
