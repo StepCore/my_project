8d630e634d44be89951f945c839aa4ffde4e8a5b
Stepan <galantov251016@gmail.com>
2024-08-02 15:53:50 +0500

    create new branch 12_1

--- src/external_api.py
+++ src/external_api.py
@@ -1,18 +1,26 @@
 import requests
+from utils import json_data
 
-amount = 845
-fromm = 'RUB'
-to = 'EUR'
-url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={fromm}&amount={amount}"
+def converter_value():
+  to = 'RUB'
+  fromm = 'USD'
+  for dict_data in json_data():
+    if dict_data.get("operationAmount",{}).get("currency",{}).get("code") == 'USD':
+      amount = dict_data.get("operationAmount", {}).get('amount')
+      url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={fromm}&amount={amount}"
+      payload = {}
+      headers= {
+        "apikey": "FJ3E7A57QzroJzYxRO1CGs7xrUELYDNM"
+        }
+      response = requests.request("GET", url, headers=headers, data = payload)
+      status_code = response.status_code
+      result = response.text
+      return dict(result).get('query',{}).get('amount')
 
-payload = {}
-headers= {
-  "apikey": "FJ3E7A57QzroJzYxRO1CGs7xrUELYDNM"
-}
+print(converter_value())
+def return_sum_transaction():
+  for dict_data in json_data():
+    if dict_data.get("operationAmount",{}).get("currency",{}).get("code") == 'RUB':
+      print(dict_data['operationAmount']['amount'])
 
-response = requests.request("GET", url, headers=headers, data = payload)
-
-status_code = response.status_code
-result = response.text
-
-print(result)
+print(return_sum_transaction())
