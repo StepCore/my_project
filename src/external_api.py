26d41f1a759d3002eda8009d456c7429c7bd6c1b
Stepan <galantov251016@gmail.com>
2024-08-06 20:50:35 +0500

    complate func external_api and practice

--- src/external_api.py
+++ src/external_api.py
@@ -1,26 +1,35 @@
 import requests
+import json
 from utils import json_data
 
+list_transaction = []
 def converter_value():
-  to = 'RUB'
-  fromm = 'USD'
-  for dict_data in json_data():
-    if dict_data.get("operationAmount",{}).get("currency",{}).get("code") == 'USD':
-      amount = dict_data.get("operationAmount", {}).get('amount')
-      url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={fromm}&amount={amount}"
-      payload = {}
-      headers= {
-        "apikey": "FJ3E7A57QzroJzYxRO1CGs7xrUELYDNM"
-        }
-      response = requests.request("GET", url, headers=headers, data = payload)
-      status_code = response.status_code
-      result = response.text
-      return dict(result).get('query',{}).get('amount')
+    list_converted = []
+    for summ in list_transaction:
+        if 'USD' in summ:
+            amount = summ[:-4]
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={amount}"
+            payload = {}
+            headers = {
+              "apikey": "jqhlspps8Cks9V3f4XwGI5qZrXx5gcpj"
+              }
+            response = requests.request("GET", url, headers=headers, data = payload)
+            status_code = response.status_code
+            result = json.loads(response.text)
+            list_transaction[list_transaction.index(summ)] = result['result']
+    return list_transaction
 
-print(converter_value())
+
 def return_sum_transaction():
-  for dict_data in json_data():
-    if dict_data.get("operationAmount",{}).get("currency",{}).get("code") == 'RUB':
-      print(dict_data['operationAmount']['amount'])
+    for dict_data in json_data():
+        if dict_data.get("operationAmount",{}).get("currency",{}).get("code") == 'RUB':
+            list_transaction.append(dict_data['operationAmount']['amount'])
+        elif dict_data.get("operationAmount", {}).get("currency", {}).get("code") == 'USD':
+            list_transaction.append(dict_data['operationAmount']['amount'] + ' ' + 'USD')
+    return list_transaction
 
-print(return_sum_transaction())
+
+return_sum_transaction()
+converter_value()
+
+print(*list_transaction, sep='\n')
