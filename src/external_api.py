ae4e066e62f4f2388e7a9a37aae3b71f64611115
Stepan <galantov251016@gmail.com>
2024-08-09 13:27:34 +0500

    broken tests

--- src/external_api.py
+++ src/external_api.py
@@ -4,46 +4,22 @@ import os
 import requests
 from dotenv import load_dotenv
 
-from src.utils import json_data
-
 load_dotenv("../.env")
 
 HEADERS = json.loads(os.getenv("HEADERS"))
 
-list_transaction = []
 
+def convert_to_rub(transaction: dict) -> float:
+    """Функция конвертации валюты в рубли"""
+    amount = transaction["operationAmount"]["amount"]
+    currency = transaction["operationAmount"]["currency"]["code"]
 
-def return_sum_transaction():
-    """Функция, которая возвращает список сумм транзакций с пометками"""
-    for dict_data in json_data():
-        if (
-            dict_data.get("operationAmount", {}).get("currency", {}).get("code")
-            == "RUB"
-        ):
-            list_transaction.append(dict_data["operationAmount"]["amount"])
-        elif (
-            dict_data.get("operationAmount", {}).get("currency", {}).get("code")
-            == "USD"
-        ):
-            list_transaction.append(
-                dict_data["operationAmount"]["amount"] + " " + "USD"
-            )
-    return list_transaction
-
-
-def converter_value():
-    """Апи которое возвращает заданную сумму в рублях"""
-    for summ in list_transaction:
-        if "USD" in summ:
-            amount = summ[:-4]
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={amount}"
-            payload = {}
-            response = requests.request("GET", url, headers=HEADERS, data=payload)
-            status_code = response.status_code
-            result = json.loads(response.text)
-            list_transaction[list_transaction.index(summ)] = result["result"]
-        return list_transaction
-
-
-return_sum_transaction()
-converter_value()
+    if currency == "RUB":
+        return float(amount)
+    else:
+        response = requests.get(
+            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}",
+            headers=HEADERS,
+        )
+        data = response.json()
+        return float(data["result"])
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,40 +1,29 @@
-from unittest.mock import patch
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
-from src.utils import json_data
-import pytest
-
-from ..src.external_api import converter_value, return_sum_transaction
+from ..src.external_api import convert_to_rub
 
 
-def test_return_sum_transaction(out_list_transaction):
-    assert return_sum_transaction() == out_list_transaction
-
-
-def test_converter_value_1():
+def test_convert_to_rub():
     mock_converter = Mock(return_value="9824.07")
-    converter_value = mock_converter
-    assert converter_value == "9824.07"
+    convert_to_rub = mock_converter
+    assert convert_to_rub == "9824.07"
     mock_converter.assert_called_once_with()
 
 
 @patch("src.utils.get_exchange_rate")
 def test_converter_value(mock_get_exchange_rate):
-    mock_get_exchange_rate.return_value = (True, 568457.78)
+    mock_get_exchange_rate.return_value = (True, 5684.78)
     usd_transaction = {
         "id": 41428829,
         "state": "EXECUTED",
         "date": "2019-07-03T18:35:29.512364",
         "operationAmount": {
-            "amount": "8221.37",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
+            "amount": "5684.78",
+            "currency": {"name": "руб", "code": "RUB"},
         },
         "description": "Перевод организации",
         "from": "MasterCard 7158300734726758",
-        "to": "Счет 35383033474447895560"
+        "to": "Счет 35383033474447895560",
     }
-    result = converter_value(usd_transaction)
-    assert result == 568457.78
+    result = convert_to_rub(usd_transaction)
+    assert result == 5684.78
