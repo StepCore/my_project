bef92394a6cd28637f66ef773430c5cdb9659319
Stepan <galantov251016@gmail.com>
2024-08-18 21:11:21 +0500

    create pandas and csv file

---
+++ src/pandas_&_CSV.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import csv
+
+
+def csv_reader(read_file):
+    with open(read_file, encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
+
+
+# print(csv_reader('../transactions.csv'))
+
+
+def excel_reader(read_file):
+    reader = pd.read_excel(read_file)
+    print(dict(reader))
+
+
+print(excel_reader('../transactions_excel.xlsx'))
