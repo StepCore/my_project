6f30f81bde2bb7e7af7f792f082a591bc9c827b7
Stepan <galantov251016@gmail.com>
2024-08-22 20:43:34 +0500

    create filter_by_description

--- src/filter_transactions.py
+++ src/filter_transactions.py
@@ -1,17 +1,42 @@
 import re
+from collections import Counter
 from src.pandas_and_CSV import csv_reader
 
 
-def filter_transaction(string='id'):
+def filter_transaction(string="id"):
     filtered_list = []
     pattern = re.compile(string, flags=re.IGNORECASE)
-    list_transaction = csv_reader(read_file='../transactions.csv')
+    list_transaction = csv_reader(read_file="../transactions.csv")
     for item in list_transaction:
-        str_item = str(item)
-        matches = pattern.findall(str_item)
+        matches = pattern.findall(str(item))
         for match in matches:
-            filtered_list.append(str_item)
+            filtered_list.append(item)
     return filtered_list
 
 
-print(*filter_transaction('19626'), sep='\n')
+# print(*filter_transaction(), sep='\n')
+
+
+def filter_by_description(descriptions):
+    filtered_transactions = [
+        transaction
+        for transaction in filter_transaction("id")
+        if transaction["description"] in descriptions
+    ]
+    description_counts = Counter(
+        transaction["description"] for transaction in filtered_transactions
+    )
+
+    return description_counts
+
+
+print(
+    filter_by_description(
+        [
+            "Перевод организации",
+            "Перевод с карты на карту",
+            "Открытие вклада",
+            "Перевод со счета на счет",
+        ]
+    )
+)
--- src/pandas_and_CSV.py
+++ src/pandas_and_CSV.py
@@ -4,22 +4,19 @@ import pandas as pd
 
 
 def csv_reader(read_file):
-    '''Функция, читающая CSV файл'''
-    transaction = []
-    with open(read_file, mode='r', encoding="utf-8") as file:
-        reader = csv.DictReader(file, delimiter=';')
-        for row in reader:
-            transaction.append(row)
-    return transaction
+    """Функция, читающая CSV файл"""
+    with open(read_file, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        return list(reader)
 
 
 # print(csv_reader('../transactions.csv'))
 
 
 def excel_reader(read_file):
-    '''Функция, читающая EXCEL файл'''
+    """Функция, читающая EXCEL файл"""
     reader = pd.read_excel(read_file)
-    return reader.to_dict(orient='records')
+    return reader.to_dict(orient="records")
 
 
 # print(excel_reader('../transactions_excel.xlsx'))
