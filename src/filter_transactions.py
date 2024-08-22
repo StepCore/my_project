3273b5820ab0916a3cafa7af4fd3c3d1d3e7f54c
Stepan <galantov251016@gmail.com>
2024-08-22 16:25:16 +0500

    create filter_transactions

--- src/filter_transactions.py
+++ src/filter_transactions.py
@@ -1,5 +1,17 @@
 import re
+from src.pandas_and_CSV import csv_reader
 
-def filter_transaction(string):
-    pattern = re.compile(string)
-    matches = pattern.finditer(csv_reader())
+
+def filter_transaction(string='id'):
+    filtered_list = []
+    pattern = re.compile(string, flags=re.IGNORECASE)
+    list_transaction = csv_reader(read_file='../transactions.csv')
+    for item in list_transaction:
+        str_item = str(item)
+        matches = pattern.findall(str_item)
+        for match in matches:
+            filtered_list.append(str_item)
+    return filtered_list
+
+
+print(*filter_transaction('19626'), sep='\n')
--- src/pandas_and_CSV.py
+++ src/pandas_and_CSV.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def csv_reader(read_file):
+    '''Функция, читающая CSV файл'''
     transaction = []
     with open(read_file, mode='r', encoding="utf-8") as file:
         reader = csv.DictReader(file, delimiter=';')
@@ -16,6 +17,7 @@ def csv_reader(read_file):
 
 
 def excel_reader(read_file):
+    '''Функция, читающая EXCEL файл'''
     reader = pd.read_excel(read_file)
     return reader.to_dict(orient='records')
 
